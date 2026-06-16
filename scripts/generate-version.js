4eacf4b61fc62f0ccabfc01eac266a632c82a86c
sensorbuilder <72987674+sensorbuilder@users.noreply.github.com>
2026-06-16 15:58:18 +0000

    update scripts/generate-version.js to include version information in the version.json file

--- scripts/generate-version.js
+++ scripts/generate-version.js
@@ -3,11 +3,12 @@ import fs from 'fs';
 
 const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 const commitDate = execSync('git log -1 --format=%cd --date=short').toString().trim();
+const version = 'v2'
 
 const versionInfo = {
   commit: commitHash,
   date: commitDate,
-  message: "alpha"};
+  message: version};
 
 fs.writeFileSync(
   './src/version.json',
